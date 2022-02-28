0dc07dc2468134d4559925eb315f867cae5f01a9
Saahil <saahil@cred.club>
2022-02-28 16:40:48 +0530

    Removed static keyspace from cassandra CQL queries

--- kong/plugins/cluster-stats/repo.lua
+++ kong/plugins/cluster-stats/repo.lua
@@ -18,8 +18,8 @@ then
   NUM_NODES_SQL_TEMPLATE = "SELECT COUNT(1) FROM cluster_stats_heartbeat WHERE EXTRACT(EPOCH FROM updated_at) > %d"
 else
   -- Queries for cassandra 
-  CLEANUP_SQL_TEMPLATE = "DELETE FROM kong_island.cluster_stats_heartbeat WHERE updated_at > %d ALLOW FILTERING;"
-  NUM_NODES_SQL_TEMPLATE = "SELECT COUNT(1) FROM kong_island.cluster_stats_heartbeat WHERE updated_at > %d ALLOW FILTERING;"
+  CLEANUP_SQL_TEMPLATE = "DELETE FROM cluster_stats_heartbeat WHERE updated_at > %d ALLOW FILTERING;"
+  NUM_NODES_SQL_TEMPLATE = "SELECT COUNT(1) FROM cluster_stats_heartbeat WHERE updated_at > %d ALLOW FILTERING;"
 end
 
 
